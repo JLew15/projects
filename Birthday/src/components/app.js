70d221c4daa5f420d4ee3b15da3c729ce93e905a
JLew15 <zombears@icloud.com>
2018-07-18 09:57:15 -0600

    Fonts, variables, and Large Date and Remaining label.

--- Birthday/src/components/app.js
+++ Birthday/src/components/app.js
@@ -2,9 +2,37 @@ import React, { Component } from 'react';
 import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
+import ChangeDate from './changeDate';
+import LargeText from './largeText';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: false
+    }
+  }
+
+  renderItems = function() {
+    if(this.state.active) {
+      return [
+        <Clock/>,
+        ChangeDate('Change Date', () => this.setState({ active: false }))
+        LargeText('04/03')
+        <label className="grid__remaining">Remaining until your 21st birthday</label>
+      ]
+    } else {
+      return [
+        <Picker/>,
+        Button('Generate Countdown', () => this.setState({ active: true }))
+      ]
+    }
+  }.bind(this)
+
   render() {
+
     return (
       <div className="grid">
       <h1 className="grid__title">Birthday Countdown</h1>
@@ -16,9 +44,8 @@ export default class App extends Component {
       <div className="grid__skew-light-two"></div>
       <div className="grid__skew-light-three-box"></div>
 
-      <Picker/>
-      { Button('Generate Countdown') }
-      <Clock/>
+
+      { this.renderItems() }
       </div>
     );
   }
